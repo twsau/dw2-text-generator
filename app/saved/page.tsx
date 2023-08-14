7ede230d5586b9b07d75929cf1c7025bfe614995
twsau <contact.twsau@gmail.com>
2023-08-14 22:13:08 +0100

    rainbow save page

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -1,22 +1,26 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SavedMessages } from "./Messages";
 
+const formatters: Formatter[] = ["standard", "achievement", "rainbow"];
+
 export default function Saved() {
   return (
     <Tabs defaultValue="standard">
       <div className="flex flex-col items-center">
         <h2 className="text-center mb-3">message type</h2>
         <TabsList>
-          <TabsTrigger value="standard">standard</TabsTrigger>
-          <TabsTrigger value="achievement">achievement</TabsTrigger>
+          {formatters.map((type) => (
+            <TabsTrigger key={`${type}-trigger`} value={type}>
+              {type}
+            </TabsTrigger>
+          ))}
         </TabsList>
       </div>
-      <TabsContent value="standard">
-        <SavedMessages type="standard" />
-      </TabsContent>
-      <TabsContent value="achievement">
-        <SavedMessages type="achievement" />
-      </TabsContent>
+      {formatters.map((type) => (
+        <TabsContent key={`${type}-content`} value={type}>
+          <SavedMessages type={type} />
+        </TabsContent>
+      ))}
     </Tabs>
   );
 }
